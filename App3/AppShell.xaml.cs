299bcac2d950418e268588e45743205e9dfdcc37
Nabil Akhlaque <82346725+nabilakhlaque@users.noreply.github.com>
2023-03-26 21:25:23 +0200

    Add files via upload

---
+++ App3/AppShell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using App3.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute("Page1", typeof(Page1));
+            Routing.RegisterRoute("Page2", typeof(Page2));
+            Routing.RegisterRoute("Page3", typeof(Page2));
+            Routing.RegisterRoute("Page4", typeof(Page4));
+        }
+
+    }
+}
---
+++ App3/Views/Page4.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page4 : ContentPage
+    {
+        public Page4()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"..");
+        }
+    }
+}
